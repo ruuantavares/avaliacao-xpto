cb9f322967fa10255a6362fcc4c4e1ec010a7d09
Ruan <ruuantavares@gmail.com>
2025-09-08 20:48:15 -0300

    criado o buscador de api na pasta API

---
+++ src/api/ano.jsx
@@ -0,0 +1,11 @@
+import api from "./api"
+
+export const getCuriosidades = async () => {
+    const response = await api.get('/random/year')
+
+    if(response.status !== 200) {
+        return []
+    }
+
+    return response.data.results
+}
