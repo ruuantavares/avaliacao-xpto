1545ff26e9238104f4c850229bcae28ca28477b5
Ruan <ruuantavares@gmail.com>
2025-09-10 21:34:51 -0300

    criado pagina music

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,13 @@
 import { Link } from 'react-router-dom'
 import './style.css'
+import Header from '../../components/Header'
 
 export default function Home() {
     return (
         <>
+        <Header/>
        <main>
+        
         <h2>Gostaria de saber mais sobre os numeros?</h2>
 
         <p>Voce sabia que cada numero tem uma historia? <Link to={"Curiosidades"}><button>Saiba Mais</button></Link></p>
